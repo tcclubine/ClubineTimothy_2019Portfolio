dfe0b5bd7e98b2ab3619f5cd2b459f382181d289
Timothy Clubine <tcclubine@student.fullsail.edu>
2019-10-10 14:24:35 -0600

    created Warrior subClass

--- ClubineTimothy_MonsterSlayer/ClubineTimothy_MonsterSlayer/Hero.cs
+++ ClubineTimothy_MonsterSlayer/ClubineTimothy_MonsterSlayer/Hero.cs
@@ -6,20 +6,20 @@ using System.Threading.Tasks;
 
 namespace ClubineTimothy_MonsterSlayer
 {
-    abstract class Hero : IStatusChange
+    abstract class Hero 
     {
         private string mName;
         private List<Monster> mMonsterList;
         private int mHealth;
         private int mAttack;
         private int mArmor;
-        private bool mStatus;
+        
 
         public string Name { get { return mName; } set { mName = value; } }
         public int Health { get { return mHealth; }  }
         public int Attack { get { return mAttack; }  }
         public int Armor { get { return mArmor; }  }
-        public bool Status { get { return mStatus; } }
+        
         public List<Monster> Monsters { get { return mMonsterList; } }
 
         public Hero(string name, int health, int attack, int armor)
@@ -28,7 +28,7 @@ namespace ClubineTimothy_MonsterSlayer
             mHealth = health;
             mAttack = attack;
             mArmor = armor;
-            mStatus = true;
+            
         }
 
         virtual public int Skill()
@@ -39,9 +39,6 @@ namespace ClubineTimothy_MonsterSlayer
             return temp;
         }
 
-        void IStatusChange.Status()
-        {
-            mStatus = !mStatus;
-        }
+        
     }
 }
---
+++ ClubineTimothy_MonsterSlayer/ClubineTimothy_MonsterSlayer/Warrior.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubineTimothy_MonsterSlayer
+{
+    class Warrior : Hero, IStatusChange
+    {
+        bool mStatus;
+
+        public Warrior(string name, int health, int attack, int armor) : base(name, health, attack, armor)
+        {
+            mStatus = true;
+        }
+
+        public override int Skill()
+        {
+            int SkillAttack = 50;
+            return SkillAttack;
+        }
+
+        void IStatusChange.Status()
+        {
+            mStatus = !mStatus;
+        }
+    }
+}

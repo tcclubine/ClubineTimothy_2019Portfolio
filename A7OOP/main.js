1f117039d1460143f1ae7623ce16a2c8934008d7
Timothy Clubine <tcclubine@student.fullsail.edu>
2019-10-19 20:13:07 -0600

    coding complete. code works. comments still needed.

--- A7OOP/main.js
+++ A7OOP/main.js
@@ -1,39 +1,42 @@
-class Cars {
-    constructor(year, make, model, color){
+class Vehicle {
+    constructor(year, make, model, color, engineSize, transmission, numOfDoors, seatingCapacity){
 
         do {
             if (year > 0) {
                 this._year = year;
                 this._make = make;
                 this._model = model;
-                this._color = color
+                this._color = color;
+                this._engineSize = engineSize;
+                this._transmission = transmission;
+                this._numOfDoors = numOfDoors;
+                this._seatingCapacity = seatingCapacity;
+                break;
+
             }else
             {
                 alert('Year is invalid.');
                 year = prompt('Enter the year of the vehicle.');
             }
 
-        }while(year <= 0)
+        }while(year < 0 || this._make == null)
+    };
 
+    toString(){
+        return '('+ this._year + ', ' + this._make + ', ' + this._model + ', ' + this._color + ', ' + this._engineSize + ', ' + this._transmission + ', ' + this._numOfDoors + ',' + this._seatingCapacity + ')';
+    }
+};
 
-
-
+class SportCar extends Vehicle{
+    constructor(year, make, model, color, engineSize, transmission, numOfDoors, seatingCapacity){
+        super(year, make, model, color, engineSize, transmission, numOfDoors, seatingCapacity);
     };
 };
 
-class SportCar extends Cars(){
-    constructor(year, make, model, color, engineSize, transmission){
-        super(year, make, model, color);
-        this._engineSize = engineSize;
-        this._transmission = transmission;
-    };
+let myCar = new Vehicle(-1, 'superMake','superModel','superColor','superEngineSize','superTransmission','superDoors','superCapacity');
+console.log(myCar.toString());
 
-    Specs(){
-        console.log(year + make + model);
-    };
+myCar = new SportCar(1990,'Nissan','300zx','Red','3.0L', '5-speed manual', '2 door', 'capacity 4');
 
-};
+console.log(myCar.toString());
 
-car = new SportCar(1990,'Nissan','300zx','Red',3.0, '5-speed manual');
-
-car.Specs();
